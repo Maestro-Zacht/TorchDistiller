008bf2c966271f0f5d4a5788cb4c5c8a31c80e33
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-05-22 10:55:24 +0200

    removed alive progress options

--- SemSeg-distill/utils/train_student.py
+++ SemSeg-distill/utils/train_student.py
@@ -18,7 +18,7 @@ import warnings
 import logging
 from alive_progress import alive_it
 from alive_progress import config_handler
-config_handler.set_global(length=15, force_tty=True, file=sys.stderr)
+config_handler.set_global(length=15)
 
 
 WORLD_SIZE = int(os.environ.get("WORLD_SIZE", 1))
